9ab8c7d746397d0be13f14a0da1bd29129c96094
Erik Torsner <erik@torgesta.com>
2015-11-07 16:12:45 +0100

    some needed restructuring

--- src/Import.php
+++ src/Import.php
@@ -12,14 +12,23 @@ class Import
     private $resolver;
     private static $self = false;
 
-    private $metaReferenceNames = array(
+    private $metaPostReferenceNames = array(
         '_thumbnail_id',
     );
-    private $postReferenceNames = array(
+    private $metaTermReferenceNames = array(
     );
-    private $optionReferenceNames = array(
+
+    private $postPostReferenceNames = array(
+    );
+
+    private $postTermReferenceNames = array(
+    );
+
+    private $optionPostReferenceNames = array(
         'page_on_front',
-        'rcp_settings' => "['foobar']",
+    );
+
+    private $optionTermReferenceNames = array(
     );
 
     public static function getInstance()
@@ -45,20 +54,24 @@ class Import
 
         $this->importSettings();
         $this->importContent();
-        $this->resolveReferences();
+
+        // references
+        $this->resolveMetaReferences();
+        $this->resolvePostReferences();
+        $this->resolveOptionReferences();
     }
 
     private function importSettings()
     {
         $wpcmd = $this->bootstrap->getWpCommand();
 
-        $cmd = $wpcmd.'config pull wpbootstrap';
-        exec($cmd);
-
         $src = BASEPATH.'/bootstrap/config/wpbootstrap.json';
         $trg = $this->bootstrap->localSettings->wppath.'/wp-content/config/wpbootstrap.json';
         @mkdir(dirname($trg), 0777, true);
         copy($src, $trg);
+
+        $cmd = $wpcmd.'config pull wpbootstrap';
+        exec($cmd);
     }
 
     private function importContent()
@@ -68,44 +81,114 @@ class Import
         $menus = new Pushmenus();
     }
 
-    private function resolveReferences()
+    private function resolveMetaReferences()
     {
-        // iterate metadata on all our managed posts and menuitems
-        // and look out for stuff that might be a post reference
-        // _thumbnail_id for instance...
-
-        foreach ($this->metaReferenceNames as $refName) {
+        foreach ($this->metaPostReferenceNames as $refName) {
             foreach ($this->posts->posts as $post) {
                 if (isset($post->post->post_meta[$refName])) {
                     foreach ($post->post->post_meta[$refName] as $item) {
-                        $newId = $this->posts->findTargetPostId($item);
-                        update_post_meta($post->id, $refName, $newId, $item);
+                        $newId = $this->findTargetObjectId($item, 'posts');
+                        if ($newId != 0) {
+                            update_post_meta($post->id, $refName, $newId, $item);
+                        }
                     }
                 }
             }
         }
-        foreach ($this->postReferenceNames as $refName) {
+        foreach ($this->metaTermReferenceNames as $refName) {
             foreach ($this->posts->posts as $post) {
-                if (isset($post->post->$refName)) {
-                    $newId = $this->posts->findTargetPostId($post->post->$refName);
-                    $args = array(
-                        'ID' => $post->id,
-                        $refName => $newId,
-                    );
-                    wp_update_post($args);
+                if (isset($post->post->post_meta[$refName])) {
+                    foreach ($post->post->post_meta[$refName] as $item) {
+                        $newId = $this->findTargetObjectId($item, 'terms');
+                        if ($newId != 0) {
+                            update_post_meta($post->id, $refName, $newId, $item);
+                        }
+                    }
                 }
             }
         }
-
-        $this->resolver->resolveReferences($this->optionReferenceNames);
     }
 
-    public function findTargetPostId($target)
+    private function resolvePostReferences()
     {
-        foreach ($this->posts->posts as $post) {
-            if ($post->post->ID == $target) {
-                return $post->id;
+        foreach ($this->postPostReferenceNames as $refName) {
+            foreach ($this->posts->posts as $post) {
+                if (isset($post->post->$refName)) {
+                    $newId = $this->findTargetObjectId($post->post->$refName, 'posts');
+                    if ($newId > 0) {
+                        $args = array('ID' => $post->id, $refName => $newId);
+                        wp_update_post($args);
+                    }
+                }
             }
+        }
+        foreach ($this->postTermReferenceNames as $refName) {
+            foreach ($this->posts->posts as $post) {
+                if (isset($post->post->$refName)) {
+                    $newId = $this->findTargetObjectId($post->post->$refName, 'terms');
+                    if ($newId > 0) {
+                        $args = array('ID' => $post->id, $refName => $newId);
+                        wp_update_post($args);
+                    }
+                }
+            }
+        }
+    }
+
+    private function resolveOptionReferences()
+    {
+        $appSettings = $this->bootstrap->appSettings;
+        if (isset($appSettings->wpbootstrap->references->posts->options)) {
+            $options = $appSettings->wpbootstrap->references->posts->options;
+            if (is_array($options)) {
+                foreach ($options as $value) {
+                    if (is_string($value)) {
+                        $this->optionPostReferenceNames[] = $value;
+                    } elseif (is_object($value)) {
+                        $arr = (array) $value;
+                        $key = key($arr);
+                        $this->optionPostReferenceNames[$key] = $arr[$key];
+                    }
+                }
+            }
+        }
+        $this->resolver->resolveOptionReferences($this->optionPostReferenceNames, 'posts');
+
+        if (isset($appSettings->wpbootstrap->references->terms->options)) {
+            $options = $appSettings->wpbootstrap->references->terms->options;
+            if (is_array($options)) {
+                foreach ($options as $value) {
+                    if (is_string($value)) {
+                        $this->optionTermReferenceNames[] = $value;
+                    } elseif (is_object($value)) {
+                        $arr = (array) $value;
+                        $key = key($arr);
+                        $this->optionTermReferenceNames[$key] = $arr[$key];
+                    }
+                }
+            }
+        }
+        $this->resolver->resolveOptionReferences($this->optionTermReferenceNames, 'terms');
+    }
+
+    public function findTargetObjectId($target, $type)
+    {
+        switch ($type) {
+            case 'posts':
+                foreach ($this->posts->posts as $post) {
+                    if ($post->post->ID == $target) {
+                        return $post->id;
+                    }
+                }
+                break;
+            case 'terms':
+                foreach ($this->taxonomies->taxonomies as $taxonomy) {
+                    foreach ($taxonomy->terms as $term) {
+                        if ($term->term->term_id == $target) {
+                            return $term->id;
+                        }
+                    }
+                }
         }
 
         return 0;
