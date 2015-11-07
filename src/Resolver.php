6b74fc1f4040be6cfa61f31da54332000083a482
Erik Torsner <erik@torgesta.com>
2015-11-07 16:15:30 +0100

    fixed some bugs found in testing, some refactoring

--- src/Resolver.php
+++ src/Resolver.php
@@ -20,25 +20,25 @@ class Resolver
         $this->import = Import::getInstance();
     }
 
-    public function resolveReferences($references)
+    public function resolveOptionReferences($references, $type)
     {
         wp_cache_delete('alloptions', 'options');
 
         foreach ($references as $key => $value) {
             if (is_integer($key)) {
                 $currentValue = get_option($value, 0);
-                $postId = $this->import->findTargetPostId($currentValue);
-                if ($postId != 0) {
-                    update_option($value, $postId);
+                $newId = $this->import->findTargetObjectId($currentValue, $type);
+                if ($newId != 0) {
+                    update_option($value, $newId);
                 }
             } elseif (is_string($key) && is_string($value)) {
                 $path = $value;
                 $currentStruct = get_option($key, 0);
                 try {
                     $currentValue = $this->getValue($currentStruct, $path);
-                    $postId = $this->import->findTargetPostId($currentValue);
-                    if ($postId != 0) {
-                        $this->setValue($currentStruct, $path, $postId);
+                    $newId = $this->import->findTargetObjectId($currentValue, $type);
+                    if ($newId != 0) {
+                        $this->setValue($currentStruct, $path, $newId);
                         update_option($key, $currentStruct);
                     }
                 } catch (\Exception $e) {
@@ -49,9 +49,9 @@ class Resolver
                 $paths = $value;
                 foreach ($paths as $path) {
                     $currentValue = $this->getValue($currentStruct, $path);
-                    $postId = $this->import->findTargetPostId($currentValue);
-                    if ($postId != 0) {
-                        $this->setValue($currentStruct, $path, $postId);
+                    $newId = $this->import->findTargetObjectId($currentValue, $type);
+                    if ($newId != 0) {
+                        $this->setValue($currentStruct, $path, $newId);
                     }
                 }
                 update_option($key, $currentStruct);
@@ -74,27 +74,8 @@ class Resolver
     {
         $value = $fld_value;
         $ret = false;
-        if (is_numeric($fld_value)) {
-            return $ret;
-        }
-
-        $b64 = $this->isBase64($fld_value);
-        if ($b64) {
-            $value = base64_decode($value);
-        }
-        $ser = $this->isSerialized($value);
-        if ($ser) {
-            $value = unserialize($value);
-        }
 
         $this->recurseSearchReplace($value, $search, $replace);
-
-        if ($ser) {
-            $value = serialize($value);
-        }
-        if ($b64) {
-            $value = base64_encode($value);
-        }
 
         if ($fld_value != $value) {
             $fld_value = $value;
@@ -120,12 +101,32 @@ class Resolver
             if (is_null($obj)) {
                 return;
             }
+
+            $b64 = $this->isBase64($obj);
+            if ($b64) {
+                $obj = base64_decode($obj);
+            }
+            $ser = $this->isSerialized($obj);
+            if ($ser) {
+                $obj = unserialize($obj);
+            }
+
             $obj =  str_replace($search, $replace, $obj);
+
+            if ($ser) {
+                $obj = serialize($obj);
+            }
+            if ($b64) {
+                $obj = base64_encode($obj);
+            }
         }
     }
 
     private function isSerialized($data)
     {
+        if (!is_string($data)) {
+            return false;
+        }
         $test = @unserialize(($data));
 
         return ($test !== false || $test === 'b:0;') ? true : false;
@@ -133,8 +134,18 @@ class Resolver
 
     private function isBase64($data)
     {
-        if (@base64_encode(base64_decode($data)) === $data) {
-            return true;
+        if (!is_string($data)) {
+            return false;
+        }
+
+        $decoded = base64_decode($data, true);
+        if (@base64_encode($decoded) === $data) {
+            $printable = ctype_print($decoded);
+            if ($printable) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
