5bf37c634fd5478e9efc00c0949cafd7b580df9c
Erik Torsner <erik@torgesta.com>
2016-01-04 19:05:46 +0100

    added docblocks

--- src/ExportBase.php
+++ src/ExportBase.php
@@ -2,22 +2,70 @@
 
 namespace Wpbootstrap;
 
+/**
+ * Class ExportBase
+ * @package Wpbootstrap
+ */
 class ExportBase
 {
+    /**
+     * @var Settings
+     */
     protected $localSettings;
+
+    /**
+     * @var Settings
+     */
     protected $appSettings;
 
+    /**
+     * @var ExportTaxonomies
+     */
     protected $exportTaxonomies;
+
+    /**
+     * @var ExportMedia
+     */
     protected $exportMedia;
+
+    /**
+     * @var ExtractMedia
+     */
     protected $extractMedia;
+
+    /**
+     * @var ExportPosts
+     */
     protected $exportPosts;
+
+    /**
+     * @var ExportSidebars
+     */
     protected $exportSidebars;
 
+    /**
+     * @var \Monolog\Logger
+     */
     protected $log;
+
+    /**
+     * @var Helpers
+     */
     protected $helpers;
+
+    /**
+     * @var Utils
+     */
     protected $utils;
+
+    /**
+     * @var string
+     */
     protected $baseUrl;
 
+    /**
+     * ExportBase constructor.
+     */
     public function __construct()
     {
         $container = Container::getInstance();
