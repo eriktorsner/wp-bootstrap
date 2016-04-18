288f47adb3861b2f956f60372d70f4290984100b
Erik Torsner <erik@torgesta.com>
2016-04-18 11:59:11 +0200

    WpCli changed to Bootstrap

--- wpcli.php
+++ wpcli.php
@@ -5,6 +5,6 @@ if (!defined('BASEPATH')) {
 }
 
 if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
-    WP_CLI::add_command('bootstrap', Wpbootstrap\WpCli::class);
+    WP_CLI::add_command('bootstrap', Wpbootstrap\Bootstrap::class);
     WP_CLI::add_command('setenv', '\Wpbootstrap\Commands\SetEnv');
 }
