da3b25a50ff5c777137a3c8af73444617bcae3fb
Erik Torsner <erik@torgesta.com>
2016-04-18 11:48:53 +0200

    new main entrypoint

---
+++ wpcli.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    define('BASEPATH', getcwd());
+}
+
+if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
+    WP_CLI::add_command('bootstrap', Wpbootstrap\WpCli::class);
+    WP_CLI::add_command('setenv', '\Wpbootstrap\Commands\SetEnv');
+}
