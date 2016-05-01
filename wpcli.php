dcf1bcd1f95c7b4a3c9b838693c7c08338ea6540
Erik Torsner <erik@torgesta.com>
2016-05-01 03:44:49 +0200

    added commands posts, taxonomies and menus

--- wpcli.php
+++ wpcli.php
@@ -6,6 +6,9 @@ if (!defined('BASEPATH')) {
 
 if (defined('WP_CLI') && class_exists('WP_CLI', false)) {
     WP_CLI::add_command('bootstrap', Wpbootstrap\Bootstrap::class);
+    WP_CLI::add_command('bootstrap posts', Wpbootstrap\Commands\Posts::class);
+    WP_CLI::add_command('bootstrap taxonomies', Wpbootstrap\Commands\Taxonomies::class);
+    WP_CLI::add_command('bootstrap menus', Wpbootstrap\Commands\Menus::class);
     WP_CLI::add_command('setenv', '\Wpbootstrap\Commands\SetEnv');
     WP_CLI::add_command('optionsnap', Wpbootstrap\Commands\OptionSnap::class);
 }
